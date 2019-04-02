b462b8cbcaa062d2202156e7687419de6d66e82e
Anna_Bondarchuk <Anna.Bondarchuk@meteogroup.com>
2019-04-02 12:09:11 +0300

    new output functionality

--- app/ImagesPrinter.php
+++ app/ImagesPrinter.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace App;
 
-class ImagesPrinter implements Printer {
+class ImagesPrinter implements Printer
+{
 
     protected $images;
 
@@ -21,10 +23,16 @@ class ImagesPrinter implements Printer {
     {
         foreach ($this->images as $image) {
             $image->rotate(180);
-
-            echo "File name: ". $image->getName() . ";";
-            echo " Width: ".  $image->getWidth() . ";";
-            echo " Height: ". $image->getheight() . ";";
+            if (preg_match("/^(.*)_(\d+)x(\d+).*/", $image->getName(), $matches)) {
+                echo "File name: " . $matches[0] . ";";
+                echo " Width: " . $matches[2] . ";";
+                echo " Height: " . $matches[3] . ";";
+                echo "\n";
+                break;
+            }
+            echo "File name: " . $image->getName() . ";";
+            echo " Width: " . $image->getWidth() . ";";
+            echo " Height: " . $image->getheight() . ";";
             echo "\n";
         }
     }
